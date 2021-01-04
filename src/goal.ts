ec799226682e0231419b25cb7c04710420ad7b65
Mikaela Andersson <mikaelaaand@gmail.com>
2021-01-04 10:52:53 +0100

    cleaned up unused code

--- src/goal.ts
+++ src/goal.ts
@@ -12,18 +12,10 @@ class Goal {
             this.goal = createSprite(this.positionX, this.positionY, 50, 50);
             this.goal.addImage(snowBall);
             this.goal.setSpeed(this.maxSpeed, 30);
-            this.goal.setCollider('circle', 0, 0, 100)
+            this.goal.setCollider('circle', 0, 0, 105)
         }
 
-        public shrink() {
-            if (mouseIsPressed) { //byt ut mouseIsPressed till bounce()
-               this.goal.scale -= 0.2
-               this.goal.setSpeed(this.maxSpeed, 30);
-               if (this.goal.scale < 0.2) {
-                    this.goal.scale = 0;
-               }
-            } 
-        }
+    
 
     bounce(sprite: any){
         this.goal.bounce(sprite)
@@ -47,7 +39,6 @@ class Goal {
     
     public draw() {
         drawSprites();
-        this.shrink();
         background(0);
        
     }
