b97c90e689e4e78b6cca9cd76f1462bf47503a8f
feliciavonbraun <feliciavonbraun@hotmail.com>
2021-01-08 15:42:41 +0100

    play pause kind of works

--- src/startscreen.ts
+++ src/startscreen.ts
@@ -1,11 +1,12 @@
 class StartScreen {
     private startGameCallback: Function
     private text: string 
-
+    private prevIsDownP: boolean
 
     constructor(startGameCallback: Function) {
         this.startGameCallback = startGameCallback;
         this.text = 'Please press SPACE to start the game'
+        this.prevIsDownP = false; 
     }
 
     pause(speed:any){
@@ -16,14 +17,19 @@ class StartScreen {
         if (keyIsDown(32)) {
             this.startGameCallback();
         }
-    
-        if (keyIsDown(80)) {
-            // material icons volume_off
-            soundes.backgroundMusic.pause()
+        
+        let muteKeyWasPressed = !this.prevIsDownP && keyIsDown(80) 
+        if (muteKeyWasPressed) {
+            if (soundes.backgroundMusic.isPaused()) {
+                soundes.backgroundMusic.play()
+            } else {
+                soundes.backgroundMusic.pause()
+
+            }
         } 
-       
-
-
+        this.prevIsDownP = keyIsDown(80) 
+        
+        // material icons volume_off
     
     }
 
