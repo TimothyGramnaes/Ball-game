af3a14f7d3581c88b31238077ef4306dffb87fbc
Mikaela Andersson <mikaelaaand@gmail.com>
2021-01-12 14:29:45 +0100

    cleaned code

--- src/ball.ts
+++ src/ball.ts
@@ -1,54 +1,31 @@
 class Ball {
-        
-    ball: any
-    radius: any
-    position: any
-    speed: number
+    private ball: any;
+    private radius: number;
+    private speed: number;
 
-            
     constructor() {
-        this.radius = 30
-        this.position = createVector(200, 200)
-                
-        this.ball = createSprite(this.position.x, this.position.y, this.radius, this.radius);
-        this.ball.setCollider('circle', 0, 0, 27); 
-
-        this.ball.addImage(imageBall) 
+        this.radius = 30;
+        this.ball = createSprite(200, 200, this.radius, this.radius);
+        this.ball.setCollider('circle', 0, 0, 27);
+        this.ball.addImage(imageBall)
         this.speed = 10;
         this.ball.setSpeed(this.speed, Math.random() * 360)
-        
-        // this.velocity = createVector(this.speed, -this.speed);
-
     }
-
 
     public draw() {
         drawSprite(this.ball);
     }
 
     bounce(sprite: any) {
-
-        
         this.ball.setSpeed(this.speed)
-
-        if(this.ball.bounce(sprite)) {
+        if (this.ball.bounce(sprite)) {
             sounds.ballCollide.play();
         }
-         
-        
-        
-
-
-
-    }
-
-    public update() {
-
     }
 
     bounceWalls(sprite: any) {
-            if(this.ball.bounce(sprite)) {
-                sounds.ballCollide.play();
-            }
-    }    
+        if (this.ball.bounce(sprite)) {
+            sounds.ballCollide.play();
+        }
+    }
 }
